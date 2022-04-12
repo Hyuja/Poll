1e90575997caee38c0d7f81a10398924b19467a0
sungjun4403 <geulligu89@naver.com>
2022-04-13 03:10:53 +0900

    .

--- POLL/Pollpjct/userapp/models.py
+++ POLL/Pollpjct/userapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class useraccount (models.Model, AbstractBaseUser):
+    votername = models.CharField(max_length = 20)
+    sex = models.CharField(max_length = 1)
+    birth = models.DateField()
+    address = models.CharField(max_length = 100)
+    votesn = models.CharField(max_length = 12)
+    ifvoted = models.BooleanField(default = False)
+    voteresult = models.CharField(max_length = 1, default = "0")
+    etc = models.TextField(null = True, blank = True)
+    
+
+
+    def __str__(self):
+        return str(self.votername) + str(self.birth)
+        
