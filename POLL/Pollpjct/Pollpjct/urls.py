b9a196cfe04a49f60c2d47d1d8addd2e69a408a3
sungjun4403 <geulligu89@naver.com>
2022-04-13 04:00:26 +0900

    .

--- POLL/Pollpjct/Pollpjct/urls.py
+++ POLL/Pollpjct/Pollpjct/urls.py
@@ -7,6 +7,8 @@ from userapp import views as usv
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', usv.home, name ="home"),
-    path('signin/', usv.userlogin, name = "userlogin")
-    #path('adminaccess/', adv.login, name = "adminlogin"),
+    path('signin/', usv.userlogin, name = "userlogin"), 
+    path('adminaccess/', adv.adminloigin, name = "adminlogin"),
+    path('adminaccess/menu', adv.menu, name = "menu")
+    
 ]
--- POLL/Pollpjct/adminapp/views.py
+++ POLL/Pollpjct/adminapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def adminloigin (request):
+    return render(request, "adminlogin.html")
+
+def menu (request):
+    return render (request, "menu.html")
